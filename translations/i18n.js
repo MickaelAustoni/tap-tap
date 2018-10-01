b52a699151938ee2fea04ada9dbbb10581409236
mickaelaustoni <mickael.austoni@parishanghai.fr>
2018-10-01 22:19:58 +0200

    * Add language (IT)

--- translations/i18n.js
+++ translations/i18n.js
@@ -2,12 +2,13 @@ import i18n from 'ex-react-native-i18n';
 import fr from './fr.json';
 import en from './en.json';
 import de from './de.json';
+import it from './it.json';
 
 i18n.initAsync().then(() => {
     // Success
 });
 
 i18n.fallbacks    = true;
-i18n.translations = {en, fr, de};
+i18n.translations = {en, fr, de, it};
 
 export default i18n;
